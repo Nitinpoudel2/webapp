db95e93a5614af6db5a489a9cdc2100cc90bff86
Nitin-Paudel <nitinpoudel2071@gmail.com>
2025-02-05 03:28:56 -0600

    Made some changes final

--- react-first-project/src/pages/login/Login.tsx
+++ react-first-project/src/pages/login/Login.tsx
@@ -77,13 +77,13 @@ const Login = () => {
           )}
           {!isLoading && (
             <button
-              className="btn btn-sm app-primary-bg-color btn-outline-light"
+              className="btn btn-sm app-primary-bg-color btn-outline-light mx-1"
               type="submit"
             >
               Login
             </button>
           )}
-          <button className="btn btn-sm app-primary-bg-color btn-light-outline" type="reset" onClick={formik.handleReset}>
+          <button className="btn btn-sm app-primary-bg-color btn-outline-light" type="reset" onClick={formik.handleReset}>
             Reset
           </button>
         </form>
